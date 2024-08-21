bdd558695a5d6ec9e20c4a5f39e1af92f7f03a68
shodeq <alfiananwarshodiqi@gmail.com>
2024-08-21 10:52:26 +0700

    modified:   e_commerce/src/pages/auth/Login.jsx         modified:   e_commerce/src/pages/auth/Register.jsx

--- e_commerce/src/pages/auth/Register.jsx
+++ e_commerce/src/pages/auth/Register.jsx
@@ -2,14 +2,57 @@ import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
-    <div>
-        <h1 className="text-8xl">Register Page</h1>
-        <div className="pt-10">
-            <span className="text-xl">Sudah punya akun dek? <Link to="/auth/login" className="text-blue-500 underline">login deeek!</Link></span>
-        </div>
-        <div>
-            <Link to={"/"} className="text-blue-500 underline">Kembali ke halaman depan</Link>
-        </div>
+      
+
+<section className="bg-gray-50 dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+      Flowbite
+    </a>
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Register new account
+        </h1>
+        <form className="space-y-4 md:space-y-6" action="#">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
+            <input type="password" id="repeat-password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required />
+          </div>
+          <div className="flex items-start mb-5">
+            <div className="flex items-center h-5">
+              <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+            </div>
+            <label className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-primary-600 hover:underline dark:text-primary-500">terms and conditions</a></label>
+          </div>
+          <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"> <Link to="/auth/login"> Register new account</Link></button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Already have an account? <Link to="/auth/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</Link>
+          </p>
+        </form>
+      </div>
     </div>
+  </div>
+</section>
+
+
+    // <div>
+    //     <h1 classNameName="text-8xl">Register Page</h1>
+    //     <div classNameName="pt-10">
+    //         <span classNameName="text-xl">Sudah punya akun dek? <Link to="/auth/login" classNameName="text-blue-500 underline">login deeek!</Link></span>
+    //     </div>
+    //     <div>
+    //         <Link to={"/"} classNameName="text-blue-500 underline">Kembali ke halaman depan</Link>
+    //     </div>
+    // </div>
   )
 }
